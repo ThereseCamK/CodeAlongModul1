cc13193a53f09fdf9cd32d8c6183f29d504ae9d3
Therese C Kjær <therese@getacademy.no>
2021-04-08 13:25:09 +0200

    Initial commit

---
+++ controller.js
@@ -0,0 +1,32 @@
+function henterUtTekstFornavn(fornavn){
+        
+    input = fornavn;
+  
+    show();
+}
+function henterUtTekstEtternavn(etternavn){
+        
+    input2 = etternavn;
+  
+    show();
+}
+// function sjekkOmTall(){
+        
+
+//     show();
+// }
+
+
+
+function checkIfBetweenZeroAndHundred() {
+    if(number <= 100 && number > 0){
+        return true;
+    }
+    return false;
+  }
+
+
+
+  function add(a, b) {
+    return a + b;
+  }
